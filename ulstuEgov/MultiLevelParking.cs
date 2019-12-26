ef0f7329d4c417649f9c519881e0d496e825b3b6
bertikq <50046409+bertikq@users.noreply.github.com>
2019-12-26 10:52:23 +0400

    del method

--- ulstuEgov/MultiLevelParking.cs
+++ ulstuEgov/MultiLevelParking.cs
@@ -72,12 +72,6 @@ namespace ulstuEgov
             return true;
         }
 
-        private void WriteToFile(string text, FileStream stream)
-        {
-            byte[] info = new UTF8Encoding(true).GetBytes(text);
-            stream.Write(info, 0, info.Length);
-        }
-
         public bool LoadData(string filename)
         {
             if (!File.Exists(filename))
